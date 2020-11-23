1f9af7f4e6f5eaa9169ce42fb2080cb412e552c4
NeX <yuraermolenko1@mail.ru>
2020-11-23 03:53:26 +0300

    header2

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
   <a href="#" onclick="history.back();return false;"><img class="logo" src="image/logo.png" alt="logo"></a>
   <nav>
     <ul class="nav_link">
-      <li><h2>ИНФО:</h2></li>
       <li><a href="#">Hookah PR</a></li>
       <li><a href="#">Проекты</a></li>
       <li><a href="#">Контакты</a></li>
