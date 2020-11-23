ba9d6b7907467eea8c7d6aa3d671b420bab869e5
LolSraf <zhuk-bogdan@bk.ru>
2020-11-23 03:21:14 +0300

    Start in work

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Project</title>
   </head>
   <body>
-
+<h1>Project</h1>
+<p>Проект</p>
   </body>
 </html>
